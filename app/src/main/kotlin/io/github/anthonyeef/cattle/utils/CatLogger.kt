9c880866a9382d5804922a8299996f3a87d3d7f9
Anthonyeef <anthonyeef@gmail.com>
2017-02-09 21:17:00 +0800

    create CatLogger for better log(need further implement later)

---
+++ app/src/main/kotlin/io/github/anthonyeef/cattle/utils/CatLogger.kt
@@ -0,0 +1,11 @@
+package io.github.anthonyeef.cattle.utils
+
+import org.jetbrains.anko.AnkoLogger
+
+/**
+ *
+ */
+interface CatLogger : AnkoLogger {
+    // TODO: self define method for log to remote
+
+}
