db5b62636af44e2098fde74ebb27ae7871e1fe0f
Anthonyeef <anthonyeef@gmail.com>
2017-02-09 21:13:32 +0800

    add some constants

--- app/src/main/kotlin/io/github/anthonyeef/cattle/constant/AppConstant.kt
+++ app/src/main/kotlin/io/github/anthonyeef/cattle/constant/AppConstant.kt
@@ -3,6 +3,11 @@ package io.github.anthonyeef.cattle.constant
 import io.github.anthonyeef.cattle.App
 
 /**
- *
+ * Application singleton
  */
 val app = App.get()
+
+/**
+ * General viewGroup id to hold fragment
+ */
+val CONTAINER_ID = 110
