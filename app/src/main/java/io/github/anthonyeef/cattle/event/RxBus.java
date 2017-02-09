4bdbf7ccbd1ff371e2c8f56e04b2dc141c8682ca
Anthonyeef <anthonyeef@gmail.com>
2017-02-09 21:11:34 +0800

    implement RxBus

---
+++ app/src/main/java/io/github/anthonyeef/cattle/event/RxBus.java
@@ -0,0 +1,27 @@
+package io.github.anthonyeef.cattle.event;
+
+import com.jakewharton.rxrelay2.PublishRelay;
+import com.jakewharton.rxrelay2.Relay;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+
+/**
+ * courtesy: https://gist.github.com/benjchristensen/04eef9ca0851f3a5d7bf
+ */
+public class RxBus {
+
+    private final Relay<Object> _bus = PublishRelay.create().toSerialized();
+
+    public void send(Object o) {
+        _bus.accept(o);
+    }
+
+    public Flowable<Object> asFlowable() {
+        return _bus.toFlowable(BackpressureStrategy.LATEST);
+    }
+
+    public boolean hasObservers() {
+        return _bus.hasObservers();
+    }
+}
