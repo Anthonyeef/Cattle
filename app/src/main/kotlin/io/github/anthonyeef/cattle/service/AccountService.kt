ab73e12d0473ff4282f243329d4889fd47ea9511
Anthonyeef <anthonyeef@gmail.com>
2017-02-12 18:10:40 +0800

    tune service

--- app/src/main/kotlin/io/github/anthonyeef/cattle/service/AccountService.kt
+++ app/src/main/kotlin/io/github/anthonyeef/cattle/service/AccountService.kt
@@ -1,7 +1,7 @@
 package io.github.anthonyeef.cattle.service
 
 import io.github.anthonyeef.cattle.service.model.UserInfo
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Url
 
@@ -13,8 +13,8 @@ interface AccountService {
     // 获取未授权的 requestToken
     // 接口地址：http://fanfou.com/oauth/access_token
     @GET
-    fun oauthAccessToken(@Url url: String): Call<String>
+    fun oauthAccessToken(@Url url: String): Observable<String>
 
     @GET("account/verify_credentials.json")
-    fun verifyCredential(): Call<UserInfo>
+    fun verifyCredential(): Observable<UserInfo>
 }
