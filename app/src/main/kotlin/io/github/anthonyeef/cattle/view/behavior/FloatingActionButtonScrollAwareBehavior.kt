1f4146c658e4ab8e0655e645689a9c1fa3520dea
Anthonyeef <anthonyeef@gmail.com>
2017-03-13 00:15:14 +0800

    HomeActivity: add compose FloatingActionButton

---
+++ app/src/main/kotlin/io/github/anthonyeef/cattle/view/behavior/FloatingActionButtonScrollAwareBehavior.kt
@@ -0,0 +1,37 @@
+package io.github.anthonyeef.cattle.view.behavior
+
+import android.content.Context
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.view.ViewCompat
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Custom FloatingActionButton behavior, hide && show when scrolling list
+ */
+class FloatingActionButtonScrollAwareBehavior : FloatingActionButton.Behavior {
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout?, child: FloatingActionButton?,
+                                     directTargetChild: View?, target: View?, nestedScrollAxes: Int): Boolean {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes)
+    }
+
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout?, child: FloatingActionButton,
+                                target: View?, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed)
+
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
+            child.hide(object : FloatingActionButton.OnVisibilityChangedListener() {
+                override fun onHidden(fab: FloatingActionButton?) {
+                    super.onHidden(fab)
+                    fab?.visibility = View.INVISIBLE
+                }
+            })
+        } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
+            child.show()
+        }
+    }
+}
