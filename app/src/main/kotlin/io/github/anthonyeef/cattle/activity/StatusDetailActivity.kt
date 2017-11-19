38bfbe84ad93fb763b4f2755f4dcdc22e778e2a2
Anthonyeef <anthonyeef@gmail.com>
2017-11-19 23:34:58 +0800

    status: add StatusDetailActivity & StatusDetailFragment

---
+++ app/src/main/kotlin/io/github/anthonyeef/cattle/activity/StatusDetailActivity.kt
@@ -0,0 +1,68 @@
+package io.github.anthonyeef.cattle.activity
+
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.Toolbar
+import io.github.anthonyeef.cattle.R
+import io.github.anthonyeef.cattle.fragment.StatusDetailFragment
+import io.github.anthonyeef.cattle.fragment.StatusDetailFragment.Companion.KEY_STATUS_ID
+import io.github.anthonyeef.cattle.presenter.StatusDetailPresenter
+import org.jetbrains.anko.findOptional
+import org.jetbrains.anko.support.v4.withArguments
+
+/**
+ *
+ */
+class StatusDetailActivity : BaseActivity() {
+
+    private val toolbar: Toolbar? by lazy { findOptional<Toolbar>(R.id.toolbar) }
+
+    companion object {
+        const val EXTRA_STATUS_ID = "extra_status_id"
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_status_detail)
+        setupToolbar()
+        setupDetailFragment()
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun setupToolbar() {
+        toolbar?.let {
+            setSupportActionBar(it)
+
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+            supportActionBar?.elevation = 4.0f
+            it.setTitle(R.string.page_title_detail)
+        }
+    }
+
+    private fun setupDetailFragment() {
+        val statusId = intent.getStringExtra(EXTRA_STATUS_ID)
+        statusId?.let {
+            var detailFragment = supportFragmentManager.findFragmentById(R.id.contentFrame) as? StatusDetailFragment
+            if (detailFragment == null) {
+                detailFragment = StatusDetailFragment().withArguments(KEY_STATUS_ID to it)
+
+                supportFragmentManager.beginTransaction()
+                        .add(R.id.contentFrame, detailFragment)
+                        .commit()
+            }
+
+            StatusDetailPresenter(detailFragment, it)
+        }
+    }
+}
---
+++ app/src/main/kotlin/io/github/anthonyeef/cattle/fragment/StatusDetailFragment.kt
@@ -0,0 +1,50 @@
+package io.github.anthonyeef.cattle.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.anthonyeef.cattle.contract.StatusDetailContract
+import io.github.anthonyeef.cattle.data.statusData.Status
+import io.github.anthonyeef.cattle.view.StatusItemDetailViewProvider
+
+/**
+ *
+ */
+class StatusDetailFragment : BaseListFragment(), StatusDetailContract.View {
+
+    companion object {
+        const val KEY_STATUS_ID = "key_status_id"
+    }
+
+    private var statusDetailPresenter: StatusDetailContract.Presenter? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        adapter.register(Status::class.java, StatusItemDetailViewProvider())
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val contentView =  super.onCreateView(inflater, container, savedInstanceState)
+        disablePullToRefresh()
+
+        return contentView
+    }
+
+    override fun showStatusDetail(item: Status) {
+        items.add(item)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        statusDetailPresenter?.subscribe()
+    }
+
+
+    override fun setPresenter(presenter: StatusDetailContract.Presenter?) {
+        statusDetailPresenter = presenter
+    }
+}
