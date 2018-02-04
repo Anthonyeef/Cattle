2f3fc7e8427e74ed251d643a07673ec087e7816b
Anthonyeef <anthonyeef@gmail.com>
2018-02-04 17:27:36 +0800

    album: add idling utils

---
+++ app/src/main/java/io/github/anthonyeef/cattle/utils/EspressoIdlingResource.java
@@ -0,0 +1,26 @@
+package io.github.anthonyeef.cattle.utils;
+
+import android.support.test.espresso.IdlingResource;
+
+/**
+ *
+ */
+
+public class EspressoIdlingResource {
+    private static final String RESOURCE = "GLOBAL";
+
+    private static SimpleCountingIdlingResource mCountingIdlingResource =
+            new SimpleCountingIdlingResource(RESOURCE);
+
+    public static void increment() {
+        mCountingIdlingResource.increment();
+    }
+
+    public static void decrement() {
+        mCountingIdlingResource.decrement();
+    }
+
+    public static IdlingResource getIdlingResource() {
+        return mCountingIdlingResource;
+    }
+}
