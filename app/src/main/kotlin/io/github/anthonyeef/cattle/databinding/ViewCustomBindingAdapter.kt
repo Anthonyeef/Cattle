f19fb15ffb9bacd7977655b4b5a0c59ce3a96936
Anthonyeef <anthonyeef@gmail.com>
2018-02-04 17:26:08 +0800

    album: add custom view binding method for album item

--- app/src/main/kotlin/io/github/anthonyeef/cattle/databinding/ViewCustomBindingAdapter.kt
+++ app/src/main/kotlin/io/github/anthonyeef/cattle/databinding/ViewCustomBindingAdapter.kt
@@ -10,14 +10,13 @@ import io.github.anthonyeef.cattle.data.statusData.Status
 import io.github.anthonyeef.cattle.extension.goneIf
 import io.github.anthonyeef.cattle.utils.StatusParsingUtils
 
-/**
- *
- */
+// bind status source to TextView
 @BindingAdapter("statusSource")
 fun bindStatusSource(view: TextView, source: String) {
     StatusParsingUtils.setSource(view, source)
 }
 
+// bind parsed status content to TextView
 @BindingAdapter("status")
 fun bindStatusContent(view: TextView, status: Status) {
     if (status.text.isNotEmpty()) {
@@ -27,12 +26,24 @@ fun bindStatusContent(view: TextView, status: Status) {
     }
 }
 
+// bind large size status photo to ImageView
 @BindingAdapter("status")
 fun bindStatusPhoto(view: ImageView, status: Status) {
     view.goneIf(status.photo == null)
     if (status.photo != null) {
         Glide.with(view.context)
                 .load(status.photo?.largeurl)
+                .into(view)
+    }
+}
+
+// bind normal size photo to ImageView
+@BindingAdapter("previewPhoto")
+fun bindStatusPreviewPhoto(view: ImageView, status: Status) {
+    view.goneIf(status.photo == null)
+    if (status.photo != null) {
+        Glide.with(view.context)
+                .load(status.photo?.imageurl)
                 .into(view)
     }
 }
