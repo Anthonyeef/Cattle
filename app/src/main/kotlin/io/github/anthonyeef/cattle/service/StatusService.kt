b21bded9d8165bb5aae191917911de15d3139b31
yifen <anthonyeef@gmail.com>
2018-06-03 18:05:03 +0800

    status_detail: add StatusDetailFragmentNg for context status

--- app/src/main/kotlin/io/github/anthonyeef/cattle/service/StatusService.kt
+++ app/src/main/kotlin/io/github/anthonyeef/cattle/service/StatusService.kt
@@ -10,12 +10,16 @@ import retrofit2.http.*
  */
 interface StatusService {
 
-    @Multipart
-    @POST("statuses/update.json")
-    fun composeNewFanfou(
-            @Part("status") status: String
-    ): Observable<Status>
+  @Multipart
+  @POST("statuses/update.json")
+  fun composeNewFanfou(
+      @Part("status") status: String
+  ): Observable<Status>
 
-    @GET("statuses/show.json")
-    fun getFanfouById(@Query("id") id: String): Flowable<Status>
+  @GET("statuses/show.json")
+  fun getFanfouById(@Query("id") id: String): Flowable<Status>
+
+  @GET("statuses/context_timeline.json")
+  fun getContextStatus(@Query("id") id: String,
+                       @Query("format") format: String = "html"): Flowable<List<Status>>
 }
